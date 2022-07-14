fa4c125bfcf755e8589ceca3bdbaa00b84cea863
Suniel Mahesh <sunil@amarulasolutions.com>
2022-07-14 16:32:45 +0530

    board: sunxi: Add it6161 initialization code
    Signed-off-by: Suniel Mahesh <sunil@amarulasolutions.com>

--- board/sunxi/board.c
+++ board/sunxi/board.c
@@ -55,6 +55,183 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
+#define IT6161_MIPI_RX 0x6C
+#define IT6161_HDMI_TX 0x4C
+
+#define INPUT_CLOCK_DELAY  0x01
+#define F_MODE_RGB444  0
+#define F_MODE_YUV422 1
+#define F_MODE_YUV444 2
+#define F_MODE_CLRMOD_MASK 3
+
+#define REG_TX_SW_RST       0x04
+#define B_TX_ENTEST    (1<<7)
+#define B_TX_REF_RST_HDMITX (1<<5)
+#define B_TX_AREF_RST (1<<4)
+#define B_HDMITX_VID_RST (1<<3)
+#define B_HDMITX_AUD_RST (1<<2)
+#define B_TX_HDMI_RST (1<<1)
+#define B_TX_HDCP_RST_HDMITX (1<<0)
+
+#define REG_TX_AVIINFO_DB1 0x58
+
+#define REG_TX_AFE_DRV_CTRL 0x61
+#define B_TX_AFE_DRV_RST    (1<<4)
+
+#define REG_TX_INPUT_MODE  0x70
+#define O_TX_INCLKDLY   0
+#define M_TX_INCLKDLY   3
+#define B_TX_INDDR          (1<<2)
+#define B_TX_SYNCEMB    (1<<3)
+#define B_TX_2X656CLK   (1<<4)
+#define B_TX_PCLKDIV2  (1<<5)
+#define M_TX_INCOLMOD   (3<<6)
+#define B_TX_IN_RGB    0
+#define B_TX_IN_YUV422 (1<<6)
+#define B_TX_IN_YUV444 (2<<6)
+
+#define T_MODE_CCIR656 (1<<0)
+#define T_MODE_SYNCEMB (1<<1)
+#define T_MODE_INDDR   (1<<2)
+#define T_MODE_PCLKDIV2 (1<<3)
+
+#define REG_TX_CSC_CTRL    0x72
+#define M_TX_CSC_SEL       3
+#define B_TX_EN_DITHER      (1<<7)
+#define B_TX_EN_UDFILTER    (1<<6)
+#define B_TX_DNFREE_GO      (1<<5)
+
+#define REG_TX_HDMI_MODE   0xC0
+#define B_TX_HDMI_MODE 1
+#define B_TX_DVI_MODE  0
+
+#define REG_TX_GCP     0xC1
+#define B_TX_SETAVMUTE        (1<<0)
+
+#define REG_TX_PKT_GENERAL_CTRL    0xC6
+#define B_TX_ENABLE_PKT    1
+#define B_TX_REPEAT_PKT    (1<<1)
+
+typedef enum {
+        PCLK_LOW = 0,
+        PCLK_MEDIUM,
+        PCLK_HIGH
+} VIDEOPCLKLEVEL;
+
+typedef struct structRegSetEntry {
+        u8 offset ;
+        u8 invAndMask ;
+        u8 OrMask ;
+} RegSetEntry;
+
+const RegSetEntry HDMITX_Init_Table[] = {
+        {0x0F, 0x40, 0x00},
+        {0x62, 0x08, 0x00},
+        {0x64, 0x04, 0x00},
+        {0x01, 0x00, 0x00},
+        {0x04, 0x20, 0x20},
+        {0x04, 0x1D, 0x1D},
+        {0x01, 0x00, 0x00},
+        {0x0F, 0x01, 0x00},
+        {0xA9, 0x80, 0x80},
+        {0xBF, 0x80, 0x80},
+        {0xF8, 0xFF, 0xC3},
+        {0xF8, 0xFF, 0xA5},
+        {0xF4, 0x0C, 0x00},
+        {0xF3, 0x02, 0x00},
+        {0xF8, 0xFF, 0xFF},
+        {0x5A, 0x0C, 0x0C},
+        {0xD1, 0x0A, 0x02},
+        {0x5D, 0x04, 0x04},
+        {0x65, 0x03, 0x00},
+        {0x71, 0xF9, 0x18},
+        {0xCF, 0xFF, 0x00},
+        {0xd1, 0x02, 0x00},
+        {0x59, 0xD0, 0x40},
+        {0xE1, 0x20, 0x20},
+        {0xF5, 0x40, 0x00},
+        {0x05, 0xC0, 0x40},
+        {0x0C, 0xFF, 0xFF},
+        {0x0D, 0xFF, 0xFF},
+        {0x0E, 0x03, 0x03},
+        {0x0C, 0xFF, 0x00},
+        {0x0D, 0xFF, 0x00},
+        {0x0E, 0x02, 0x00},
+        {0x20, 0x01, 0x00},
+        {0, 0, 0}
+};
+
+const RegSetEntry HDMITX_DefaultVideo_Table[] = {
+        {0x72, 0xff, 0x00},
+        {0x70, 0xff, 0x00},
+        {0x0F, 0x01, 0x00},
+        {0x72, 0xFF, 0x03},
+        {0x73, 0xFF, 0x00},
+        {0x74, 0xFF, 0x80},
+        {0x75, 0xFF, 0x00},
+        {0x76, 0xFF, 0x00},
+        {0x77, 0xFF, 0x08},
+        {0x78, 0xFF, 0x53},
+        {0x79, 0xFF, 0x3C},
+        {0x7A, 0xFF, 0x89},
+        {0x7B, 0xFF, 0x3E},
+        {0x7C, 0xFF, 0x00},
+        {0x7D, 0xFF, 0x08},
+        {0x7E, 0xFF, 0x51},
+        {0x7F, 0xFF, 0x0C},
+        {0x80, 0xFF, 0x00},
+        {0x81, 0xFF, 0x00},
+        {0x82, 0xFF, 0x00},
+        {0x83, 0xFF, 0x08},
+        {0x84, 0xFF, 0x00},
+        {0x85, 0xFF, 0x00},
+        {0x86, 0xFF, 0x87},
+        {0x87, 0xFF, 0x0E},
+        {0x88, 0xF0, 0x00},
+        {0x04, 0x08, 0x00},
+        {0, 0, 0}
+};
+
+const RegSetEntry HDMITX_SetHDMI_Table[] = {
+        {0xC0, 0x01, 0x01},
+        {0xC1, 0x03, 0x03},
+        {0xC6, 0x03, 0x03},
+        {0, 0, 0}
+};
+
+const RegSetEntry HDMITX_SetDVI_Table[] = {
+        {0x0F, 0x01, 0x01},
+        {0x58, 0xFF, 0x00},
+        {0x0F, 0x01, 0x00},
+        {0xC0, 0x01, 0x00},
+        {0xC1, 0x03, 0x02},
+        {0xC6, 0x03, 0x00},
+        {0, 0, 0}
+};
+
+const RegSetEntry HDMITX_DefaultAVIInfo_Table[] = {
+        {0x0F, 0x01, 0x01},
+        {0x58, 0xFF, 0x10},
+        {0x59, 0xFF, 0x08},
+        {0x5A, 0xFF, 0x00},
+        {0x5B, 0xFF, 0x00},
+        {0x5C, 0xFF, 0x00},
+        {0x5D, 0xFF, 0x57},
+        {0x5E, 0xFF, 0x00},
+        {0x5F, 0xFF, 0x00},
+        {0x60, 0xFF, 0x00},
+        {0x61, 0xFF, 0x00},
+        {0x62, 0xFF, 0x00},
+        {0x63, 0xFF, 0x00},
+        {0x64, 0xFF, 0x00},
+        {0x65, 0xFF, 0x00},
+        {0x0F, 0x01, 0x00},
+        {0xCD, 0x03, 0x03},
+        {0, 0, 0}
+};
+
+static inline int Switch_HDMITX_Bank(struct udevice *it6161, int x);
+
 void i2c_init_board(void)
 {
 #ifdef CONFIG_I2C0_ENABLE
@@ -185,6 +362,327 @@ enum env_location env_get_location(enum env_operation op, int prio)
 	}
 
 	return ENVL_UNKNOWN;
+}
+
+static int it6161_write(struct udevice *dev, uint addr,
+                      uint8_t value)
+{
+        int err;
+
+        err = dm_i2c_write(dev, addr, &value, 1);
+
+        return err;
+}
+
+static int it6161_read(struct udevice *dev, unsigned int addr)
+{
+        uint8_t valb;
+        int err;
+
+        err = dm_i2c_read(dev, addr, &valb, 1);
+        if (err)
+                return err;
+
+        return valb;
+}
+
+static int it6161_set_bits(struct udevice *dev, uint addr,
+                         uint mask, uint value)
+{
+        uint8_t valb;
+        int err;
+
+        err = dm_i2c_read(dev, addr, &valb, 1);
+        if (err)
+                return err;
+
+        valb &= ~mask;
+        valb |= value;
+
+        err = dm_i2c_write(dev, addr, &valb, 1);
+
+        return err;
+}
+
+static void hdmitx_FireAFE(struct udevice *it6161)
+{
+        Switch_HDMITX_Bank(it6161, 0);
+        it6161_write(it6161, REG_TX_AFE_DRV_CTRL,0);
+}
+
+static inline int Switch_HDMITX_Bank(struct udevice *it6161, int x)
+{
+        return it6161_set_bits(it6161, 0x0F, 1, (x)&1);
+}
+
+static void setHDMITX_AVMute(struct udevice *it6161, u8 bEnable)
+{
+        Switch_HDMITX_Bank(it6161, 0);
+        it6161_set_bits(it6161, REG_TX_GCP,B_TX_SETAVMUTE, bEnable?B_TX_SETAVMUTE:0);
+        it6161_write(it6161, REG_TX_PKT_GENERAL_CTRL, B_TX_ENABLE_PKT|B_TX_REPEAT_PKT);
+}
+
+static void hdmitx_SetupAFE(struct udevice *it6161, VIDEOPCLKLEVEL level)
+{
+        printf("%s: pclk:%s", __func__, level ? "high" : "low");
+        it6161_write(it6161, REG_TX_AFE_DRV_CTRL,B_TX_AFE_DRV_RST);
+        switch (level) {
+        case PCLK_HIGH:
+                it6161_set_bits(it6161, 0x62, 0x90, 0x80);
+                it6161_set_bits(it6161, 0x64, 0x89, 0x80);
+                it6161_set_bits(it6161, 0x68, 0x10, 0x00);
+                it6161_set_bits(it6161, 0x66, 0x80, 0x80);
+                break ;
+        default:
+                it6161_set_bits(it6161, 0x62, 0x90, 0x10);
+                it6161_set_bits(it6161, 0x64, 0x89, 0x09);
+                it6161_set_bits(it6161, 0x68, 0x10, 0x10);
+                break ;
+        }
+        it6161_set_bits(it6161, REG_TX_SW_RST,B_TX_REF_RST_HDMITX|B_HDMITX_VID_RST,0);
+        it6161_write(it6161, REG_TX_AFE_DRV_CTRL,0);
+}
+
+static void hdmitx_SetCSCScale(struct udevice *it6161, u8 bInputMode,u8 bOutputMode)
+{
+        u8 ucData;
+
+        it6161_set_bits(it6161, 0xF, 0x10, 0x10);
+        ucData = it6161_read(it6161, REG_TX_CSC_CTRL) & ~(M_TX_CSC_SEL|B_TX_DNFREE_GO|B_TX_EN_DITHER|B_TX_EN_UDFILTER);
+        it6161_write(it6161, REG_TX_CSC_CTRL,ucData);
+}
+
+static void hdmitx_SetInputMode(struct udevice *it6161, u8 InputColorMode,u8 bInputSignalType)
+{
+        u8 ucData ;
+
+        ucData = it6161_read(it6161, REG_TX_INPUT_MODE);
+        ucData &= ~(M_TX_INCOLMOD|B_TX_2X656CLK|B_TX_SYNCEMB|B_TX_INDDR|B_TX_PCLKDIV2);
+        ucData |= INPUT_CLOCK_DELAY ;
+
+        switch(InputColorMode & F_MODE_CLRMOD_MASK) {
+        case F_MODE_YUV422:
+                ucData |= B_TX_IN_YUV422 ;
+                break ;
+        case F_MODE_YUV444:
+                ucData |= B_TX_IN_YUV444 ;
+                break ;
+        case F_MODE_RGB444:
+        default:
+                ucData |= B_TX_IN_RGB ;
+                break ;
+        }
+        if(bInputSignalType & T_MODE_PCLKDIV2)
+                ucData |= B_TX_PCLKDIV2 ;
+
+        if(bInputSignalType & T_MODE_CCIR656)
+                ucData |= B_TX_2X656CLK ;
+
+        if(bInputSignalType & T_MODE_SYNCEMB)
+                ucData |= B_TX_SYNCEMB ;
+
+        if(bInputSignalType & T_MODE_INDDR)
+                ucData |= B_TX_INDDR ;
+
+        it6161_write(it6161, REG_TX_INPUT_MODE,ucData);
+}
+
+static void hdmitx_loadregsetting(struct udevice *it6161, const RegSetEntry table[])
+{
+    int i ;
+
+    for( i = 0 ;  ; i++ )
+    {
+        if( table[i].offset == 0 && table[i].invAndMask == 0 && table[i].OrMask == 0 )
+        {
+            return ;
+        }
+        else if( table[i].invAndMask == 0 && table[i].OrMask == 0 )
+        {
+            printf("delay(%d)\n",(int)table[i].offset);
+            mdelay(table[i].offset);
+        }
+        else if( table[i].invAndMask == 0xFF )
+        {
+            printf("it6161_write(it6161, %02x,%02x)\n", (int)table[i].offset, (int)table[i].OrMask);
+            it6161_write(it6161, table[i].offset, table[i].OrMask);
+        }
+        else
+        {
+            printf("it6161_set_bits(it6161, %02x,%02x,%02x)\n", (int)table[i].offset, (int)table[i].invAndMask, (int)table[i].OrMask);
+            it6161_set_bits(it6161, table[i].offset, table[i].invAndMask, table[i].OrMask);
+        }
+    }
+}
+
+static bool HDMITX_EnableVideoOutput(struct udevice *it6161, VIDEOPCLKLEVEL level,u8 inputColorMode,u8 outputColorMode,u8 bHDMI)
+{
+
+    it6161_write(it6161, REG_TX_SW_RST, B_HDMITX_VID_RST|B_HDMITX_AUD_RST|B_TX_AREF_RST|B_TX_HDCP_RST_HDMITX);
+
+    Switch_HDMITX_Bank(it6161, 1);
+    it6161_write(it6161, REG_TX_AVIINFO_DB1, 0x00);
+    Switch_HDMITX_Bank(it6161, 0);
+
+    if(bHDMI)
+    {
+        setHDMITX_AVMute(it6161, true);
+    }
+    hdmitx_SetInputMode(it6161, inputColorMode, 0);
+
+    hdmitx_SetCSCScale(it6161, inputColorMode, outputColorMode);
+
+    if(bHDMI)
+    {
+        it6161_write(it6161, REG_TX_HDMI_MODE, B_TX_HDMI_MODE);
+    }
+    else
+    {
+        it6161_write(it6161, REG_TX_HDMI_MODE, B_TX_DVI_MODE);
+    }
+
+    hdmitx_SetupAFE(it6161, level);
+    it6161_write(it6161, REG_TX_SW_RST, B_HDMITX_AUD_RST|B_TX_AREF_RST|B_TX_HDCP_RST_HDMITX);
+
+    hdmitx_FireAFE(it6161);
+        return true;
+}
+
+static void mipirx_reset(struct udevice *it6161)
+{
+        it6161_set_bits(it6161, 0x10, 0x0F, 0x0F);
+        mdelay(1);
+        it6161_set_bits(it6161, 0x10, 0x0F, 0x00);
+        it6161_set_bits(it6161, 0x05, 0x08, 0x08);
+        mdelay(1);
+        it6161_set_bits(it6161, 0x05, 0x08, 0x00);
+        it6161_set_bits(it6161, 0x0d, 0x02, 0x00);
+        it6161_set_bits(it6161, 0x0C, 0x0F, 0x03);
+        it6161_set_bits(it6161, 0x11, 0x3F, 0x01);
+        it6161_set_bits(it6161, 0x12, 0x03, 0x00);
+        it6161_set_bits(it6161, 0x18, 0xf7, (4<<4) | 0x03);
+        it6161_set_bits(it6161, 0x19, 0xf3, (12<<4) | 0x03);
+        it6161_set_bits(it6161, 0x20, 0xf7, 0x03);
+        it6161_set_bits(it6161, 0x21, 0x07, 0);
+
+        it6161_set_bits(it6161, 0x44, 0x3a, 0x32);
+        it6161_set_bits(it6161, 0x4B, 0x1f, 0x01);
+        it6161_write(it6161, 0x4C, 4);
+        it6161_set_bits(it6161, 0x4D, 0x01, (4>>8) & 0x01);
+        it6161_set_bits(it6161, 0x4E, 0x0C, 0x00);
+        it6161_set_bits(it6161, 0x4F, 0x03, 0x01);
+        it6161_write(it6161, 0x27, 0x3E);
+        it6161_set_bits(it6161, 0x70, 0x01, 0x01);
+        it6161_write(it6161, 0x72, 4);
+        it6161_write(it6161, 0x73, 3);
+        it6161_set_bits(it6161, 0x80, 0x40, 0x00);
+        it6161_write(it6161, 0x21, 0x00);
+        it6161_set_bits(it6161, 0x84, 0x70, 0x00);
+        it6161_set_bits(it6161, 0xA0, 0x01, 1);
+        it6161_set_bits(it6161, 0x80, 0x1F, 0x02);
+        it6161_set_bits(it6161, 0x70, 0x01, 1);
+        it6161_set_bits(it6161, 0x05, 0x02, 0x02);
+        it6161_write(it6161, 0xA1, 0x00);
+        it6161_write(it6161, 0xA2, 0x00);
+        it6161_write(it6161, 0xA3, 0x08);
+        it6161_write(it6161, 0xA5, 0x04);
+        it6161_set_bits(it6161, 0x31, 0x80, 0x00);
+        it6161_set_bits(it6161, 0x33, 0x80, 0x00);
+        it6161_set_bits(it6161, 0x35, 0x80, 0x00);
+        it6161_set_bits(it6161, 0x37, 0x80, 0x00);
+        it6161_set_bits(it6161, 0x39, 0x80, 0x00);
+        it6161_set_bits(it6161, 0x3A, 0x80, 0x00);
+        it6161_set_bits(it6161, 0x3C, 0x80, 0x00);
+        it6161_set_bits(it6161, 0x3E, 0x80, 0x00);
+        it6161_set_bits(it6161, 0x41, 0x80, 0x00);
+        it6161_set_bits(it6161, 0x43, 0x80, 0x00);
+}
+
+static void cal_rclk(struct udevice *it6161)
+{
+        u8 i;
+        int t10usint;
+        u32 sum, RxRCLK;
+
+        sum = 0;
+        for(i=0; i<5; i++)
+        {
+                it6161_set_bits(it6161, 0x94, 0x80, 0x80);
+                mdelay(100);
+                it6161_set_bits(it6161, 0x94, 0x80, 0x00);
+
+                RxRCLK = it6161_read(it6161, 0x97);
+                RxRCLK <<= 8;
+                RxRCLK += it6161_read(it6161, 0x96);
+                RxRCLK <<=8;
+                RxRCLK += it6161_read(it6161, 0x95);
+                sum += RxRCLK;
+
+        }
+        sum /= 5;
+        printf("\n");
+
+        RxRCLK = sum/100;
+        t10usint = RxRCLK/100;
+        printf("RxRCLK = %d,%03d,%03d\n",(sum*10)/1000000,((sum*10)%1000000)/1000,((sum*10)%100));
+        printf("T10usInt=0x%03X\n", (int)t10usint);
+        it6161_write(it6161, 0x91, t10usint&0xFF);
+}
+
+static void init_mipi_rx(struct udevice *it6161)
+{
+        printf("Init MIPIRX\n");
+        mipirx_reset(it6161);
+        cal_rclk(it6161);
+
+        it6161_set_bits(it6161, 0x05, 0x03, 0x00);
+        it6161_set_bits(it6161, 0x08, 0x10, 0x10);
+}
+
+static void set_ppara(struct udevice *it6161)
+{
+        it6161_set_bits(it6161, 0x05, 0x04, 0x04);
+        it6161_set_bits(it6161, 0x05, 0x04, 0x00);
+        it6161_set_bits(it6161, 0x09, 0x10, 0x10);
+}
+
+static void it6161_init(void)
+{
+        struct udevice *bus, *hdmi_tx, *mipi_rx;
+        int i2c_bus = 1, ret;
+        u8 val;
+
+        printf("start %s", __func__);
+        ret = uclass_get_device_by_seq(UCLASS_I2C, i2c_bus, &bus);
+        if (ret) {
+                printf("%s: No bus %d\n", __func__, i2c_bus);
+                return;
+        }
+
+        ret = dm_i2c_probe(bus, IT6161_MIPI_RX, 0, &mipi_rx);
+        if (ret) {
+                printf("return val it6161: %d\n", ret);
+                printf("%s: Can't find device id=0x%x, on bus %d\n",
+                        __func__, IT6161_MIPI_RX, i2c_bus);
+                return;
+        }
+
+        ret = dm_i2c_probe(bus, IT6161_HDMI_TX, 0, &hdmi_tx);
+        if (ret) {
+                printf("return val hdmi: %d\n", ret);
+                printf("%s: Can't find device id=0x%x, on bus %d\n",
+                        __func__, IT6161_HDMI_TX, i2c_bus);
+                return;
+        }
+
+        init_mipi_rx(mipi_rx);
+        hdmitx_loadregsetting(hdmi_tx, HDMITX_Init_Table);
+        hdmitx_loadregsetting(hdmi_tx, HDMITX_DefaultVideo_Table);
+        hdmitx_loadregsetting(hdmi_tx, HDMITX_SetHDMI_Table);
+        hdmitx_loadregsetting(hdmi_tx, HDMITX_DefaultAVIInfo_Table);
+        set_ppara(mipi_rx);
+        HDMITX_EnableVideoOutput(hdmi_tx, 0, 0, 0, 0);
+        setHDMITX_AVMute(hdmi_tx, false);
 }
 
 #ifdef CONFIG_DM_MMC
@@ -342,6 +840,8 @@ int board_init(void)
 	renew_dlpc_gpio();
 	renew_it6161_gpio();
 #endif
+
+	it6161_init();
 
 	eth_init_board();
 
